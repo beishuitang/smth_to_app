a955e9d2990fd19f688a5b58817329009b34e94b
santi <beishuitang@163.com>
2023-05-05 18:06:05 +0800

    增加网络下载ip数据库功能

--- src/components/IPDB.vue
+++ src/components/IPDB.vue
@@ -1,24 +1,95 @@
 <script setup lang="ts">
 import storage from '@/scripts/storage'
 import ipData from '@/scripts/ipData'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 const loaded = ipData.searcher ? ref(true) : ref(false)
-function saveIpDb() {
-  const files = document.querySelector<HTMLInputElement>('input#newsmth_IpDB')?.files
-  if (!files || !files[0]) {
-    return
+const showImport = ref(false)
+const autoDownload = ref(false)
+const installing = ref(false)
+const contentLength = ref(0)
+const receivedLength = ref(0)
+watch(autoDownload, (value) => {
+  if (value) {
+    downloadIpDb()
   }
-  storage.saveIpDB(files[0])
+})
+function downloadIpDb() {
+  installing.value = true
+  const url = 'https://www.newsmth.top/static/ipdb.xdb'
+  fetch(url)
+    .then((response) => {
+      return getBlob(response)
+    })
+    .then((blob) => {
+      return saveIpDb(blob)
+    })
+    .catch((e) => {
+      installing.value = false
+      autoDownload.value = false
+      alert(e)
+    })
+}
+async function getBlob(response: Response) {
+  const reader = response.body?.getReader()
+  const s = response.headers.get('Content-Length')
+  if (!reader || !s) {
+    throw new Error('response.body or contentLength  is null')
+  }
+  contentLength.value = parseInt(s)
+  let chunks = []
+  let done = false
+  while (!done) {
+    const { done, value } = await reader.read()
+    if (done) {
+      break
+    }
+    chunks.push(value)
+    receivedLength.value += value.length
+  }
+  let chunksAll = new Uint8Array(receivedLength.value)
+  let position = 0
+  for (let chunk of chunks) {
+    chunksAll.set(chunk, position)
+    position += chunk.length
+  }
+  return new Blob([chunksAll])
+}
+function importIpDb() {
+  installing.value = true
+  const files = document.querySelector<HTMLInputElement>('input#newsmth_IpDB')?.files
+  if (files && files[0] && files[0].name.endsWith('.xdb')) {
+    saveIpDb(files[0])
+  } else {
+    alert('导入失败,请检查文件')
+    installing.value = false
+  }
+}
+async function saveIpDb(blob: Blob) {
+  await storage.saveIpDB(blob)
+  alert('导入完成')
+  location.reload()
 }
 </script>
 <template>
   <div>
-    <h3>ip数据库</h3>
-    <span v-if="loaded" v-on:click="loaded = !loaded">已完成导入</span>
-    <div v-if="!loaded" class="border-dotted">
-      <input type="file" name="file" accept=".xdb" id="newsmth_IpDB" />
-      <button @click="saveIpDb()">导入</button>
-    </div>
+    <h3>ip地理位置</h3>
+    数据库文件:
+    <span v-if="loaded"> 已安装 </span>
+    <span v-if="!loaded">
+      <label :class="{ checked: autoDownload }">
+        自动下载
+        <input type="checkbox" :disabled="installing" v-model="autoDownload" />
+      </label>
+      <label :class="{ checked: showImport }">
+        手动导入
+        <input type="checkbox" :disabled="installing" v-model="showImport" />
+      </label>
+      <div v-if="showImport">
+        <input type="file" name="file" accept=".xdb" id="newsmth_IpDB" />
+        <button @click="importIpDb()">导入</button>
+      </div>
+      <div v-if="installing">正在下载:{{ receivedLength }} of {{ contentLength }}</div>
+    </span>
   </div>
 </template>
 
@@ -29,5 +100,15 @@ a {
 
 .border-dotted {
   border: dotted;
+}
+
+label {
+  margin-right: 1rem;
+  display: inline-block;
+  border: outset;
+}
+
+label.checked {
+  border: inset;
 }
 </style>
