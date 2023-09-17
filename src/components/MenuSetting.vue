53bac4e14e28c3dbcbf76192c43546c0b8203934
santi <beishuitang@163.com>
2023-09-18 04:21:18 +0800

    修复菜单超出屏幕问题

--- src/components/MenuSetting.vue
+++ src/components/MenuSetting.vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
 import { useAppStateStore } from '@/stores/appStateStore'
 import { fixSearchBoard } from '@/scripts/bugHandler'
-import { watch } from 'vue'
+import { ref, watch } from 'vue'
 defineProps<{
   msg?: string
 }>()
 const showState = useAppStateStore().appState.showState
 const menuEl = document.querySelector('#menu') as HTMLElement
 const loginEl = menuEl.querySelector('#u_login') as HTMLElement
+const xlistEl = menuEl.querySelector('#xlist') as HTMLElement
 const username = 'newsmth_script_username'
 const password = 'newsmth_script_password'
 const saveConfig = 'newsmth_script_pass_config'
+const list_menu = ref<HTMLElement | null>(null)
+
 menuEl.addEventListener('click', (event) => {
   const el = event.target
   if (!(el instanceof HTMLElement)) return
@@ -54,6 +57,12 @@ function fillPassword() {
   }
 }
 
+function computeMaxHeight() {
+  const d = list_menu.value?.clientHeight
+  const maxHeight = window.innerHeight - 10 - loginEl.clientHeight - (d ? d : 50)
+  xlistEl.style.maxHeight = maxHeight + 'px'
+}
+
 watch(
   () => showState.state,
   (newState, oldState) => {
@@ -74,13 +83,15 @@ const mutConfig = {
 new MutationObserver(() => {
   fillPassword()
   fixSearchBoard()
+  computeMaxHeight()
 }).observe(loginEl, mutConfig)
 fillPassword()
 fixSearchBoard()
+computeMaxHeight()
 </script>
 
 <template>
-  <nav id="xlist-menu" class="corner">
+  <nav ref="list_menu" id="xlist-menu" class="corner">
     <ul>
       <li class="slist folder-close">
         <span class="x-leaf">
@@ -101,7 +112,7 @@ body.newsmth-plus #menu {
   bottom: 0rem;
   left: 0;
   transform: translateX(-100%);
-
+  max-height: 100%;
   -webkit-transition: transform 0.4s;
   transition: transform 0.4s;
 }
@@ -125,7 +136,7 @@ body.newsmth-plus #menu {
 
 #menu .x-folder a,
 #menu .x-leaf a {
-  line-height: 28px;
+  line-height: 2.8rem;
   padding-top: unset;
 }
 
@@ -141,21 +152,20 @@ body.newsmth-plus #menu {
 }
 
 body.newsmth-plus #xlist {
+  overflow-y: auto;
   padding: 1rem 0.8rem 0 0.8rem;
   border-bottom-width: 0;
   border-radius: 0.4rem 0.4rem 0 0;
 }
 
 .newsmth-plus #xlist-menu {
-  padding: 0 0.8rem 1rem 0.8rem;
+  padding: 0 0.8rem 0.1rem 0.8rem;
+  border: solid #c3d9ff;
   border-radius: 0 0 0.4rem 0.4rem;
-  border-width: 0 0.1rem 0.1rem 0.1rem;
+  border-width: 0.1rem 0.1rem 0.1rem 0.1rem;
 }
 
-#xlist-menu {
-  padding: 0 8px 10px 8px;
-  border: solid #c3d9ff;
-  border-radius: 0 0 4px 4px;
-  border-width: 0 1px 1px 1px;
+.newsmth-plus #xlist-menu .x-leaf {
+  border-bottom: unset;
 }
 </style>
